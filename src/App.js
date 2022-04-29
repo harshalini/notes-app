6fb2cbe2ba9f9b2aba9855406a1998c12c11c60b
Harshalini <harshalinipandhare29@gmail.com>
2022-04-29 13:34:22 +0530

    suggestions from peers

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import "./App.css";
+import { Navbar } from "./components/allComp";
 import { AppRouter } from "./router/router"
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <AppRouter />
     </div>
   );
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,6 @@
-import { Navbar } from "../components/allComp"
 export const Home = () => {
     return (
         <div>
-            <Navbar />
             <img src="../assets/note-img.svg" className="note-img" />
             <p className="txt">Express your thoughts and write them down!</p>
             <button id="primary-btn">Write now</button>
